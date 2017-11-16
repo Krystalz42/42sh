820c38b6a45358f51b9ec4c2fc79206e4be41e14
krystalz42 <aroulin@student.42.fr>
2017-11-16 23:14:46 +0100

    Jobs en BG

--- srcs/execution/executing_function/op_ampersand.c
+++ srcs/execution/executing_function/op_ampersand.c
@@ -6,6 +6,6 @@
 
 uint8_t			op_ampersand(t_node *node, t_jobs *jobs, int info)
 {
-	return (execute_node(node->left, jobs, info ^ FOREGROUND) +
+	return (execute_node(node->left, jobs, (info ^ FOREGROUND) | FORCE_FORK) +
 			execute_node(node->right, jobs, info));
 }
