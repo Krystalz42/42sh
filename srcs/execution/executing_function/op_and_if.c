fd1bedf780f48cfb6dfb0d300833095b6e2ef9a5
jle-quel <jeffersonlequellec@me.com>
2017-11-19 04:18:16 +0100

    or_if && and_if en mode badass

--- srcs/execution/executing_function/op_and_if.c
+++ srcs/execution/executing_function/op_and_if.c
@@ -6,7 +6,7 @@
 /*   By: jle-quel <jle-quel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 16:18:40 by jle-quel          #+#    #+#             */
-/*   Updated: 2017/11/19 04:09:06 by jle-quel         ###   ########.fr       */
+/*   Updated: 2017/11/19 04:13:54 by jle-quel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 
 uint8_t			op_and_if(t_node *node, t_jobs *jobs, int info)
 {
-	execute_node(node->left, jobs, info);
-	var_return(-1) == 0 ? execute_node(node->right, jobs, info) : 0;
-	return (var_return(-1));
+	return (execute_node(node->left, jobs, info) == 0
+			&& execute_node(node->right, jobs, info));
 }
--- srcs/execution/executing_function/op_or_if.c
+++ srcs/execution/executing_function/op_or_if.c
@@ -6,7 +6,7 @@
 /*   By: jle-quel <jle-quel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 16:17:58 by jle-quel          #+#    #+#             */
-/*   Updated: 2017/11/19 04:05:31 by jle-quel         ###   ########.fr       */
+/*   Updated: 2017/11/19 04:16:36 by jle-quel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@
 */
 
 uint8_t			op_or_if(t_node *node, t_jobs *jobs, int info)
-{
-	execute_node(node->left, jobs, info);
-	var_return(-1) ? execute_node(node->right, jobs, info) : 0;
-	return (var_return(-1));
+{	
+	return (execute_node(node->left, jobs, info)
+			&& execute_node(node->right, jobs, info));
 }
