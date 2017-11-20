b889d3ba2410ac785da7670f541d293935c02d25
krystalz42 <aroulin@student.42.fr>
2017-11-20 18:23:26 +0100

    Yo

--- srcs/execution/tools/new_jobs.c
+++ srcs/execution/tools/new_jobs.c
@@ -14,33 +14,22 @@
 
 static t_jobs	*create_jobs(void)
 {
-	t_jobs		**addr_jobs;
-	t_jobs		*temp_jobs;
+	t_jobs		*jobs;
+	int			index;
 
-	addr_jobs = jobs_table();
-	temp_jobs = *addr_jobs;
-	if (*addr_jobs)
-	{
-		while (temp_jobs->next)
-			temp_jobs = temp_jobs->next;
-		temp_jobs->next = (t_jobs *)ft_memalloc(sizeof(t_jobs));
-		temp_jobs->next->prev = temp_jobs;
-		return (temp_jobs->next);
-	}
-	else
-	{
-		dprintf(fd_log, "Create from addr\n");
-		*addr_jobs = (t_jobs *)ft_memalloc(sizeof(t_jobs));
-		return (*addr_jobs);
-	}
+	index = MAX_CHILD - 1;
+	jobs = jobs_table();
+	while (index >= 0 && jobs[index].process == NULL)
+		index--;
+	if (index == MAX_CHILD -1 && jobs[index].process)
+		return (NULL);
+	jobs[index].index = index + 2;
+	return (jobs + index);
 }
 
 t_jobs			*new_jobs(t_jobs *jobs)
 {
 	if (jobs == NULL)
-	{
 		jobs = create_jobs();
-		jobs->index = (jobs->prev) ? jobs->prev->index + 1 : 1;
-	}
 	return (jobs);
 }
--- srcs/memdel_struct/memdel_jobs.c
+++ srcs/memdel_struct/memdel_jobs.c
@@ -12,57 +12,36 @@
 
 #include <sh.h>
 
-static void		reset_process(t_jobs **jobs)
+static void		reset_process(t_jobs *jobs)
 {
-	t_process	*process;
+	t_process *to_kill;
 
-	while ((*jobs)->process)
+	while (jobs->process)
 	{
-		process = (*jobs)->process;
-		(*jobs)->process = (*jobs)->process->next;
-		ft_memdel((void **)&process->command);
-		ft_memdel((void **)&process);
+		to_kill = jobs->process;
+		jobs->process = jobs->process->next;
+		ft_memdel((void **)&to_kill);
 	}
-	ft_memdel((void **)jobs);
+	ft_memset((void *)jobs, 0 , sizeof(t_jobs));
 }
 
-static void		connect_use(t_jobs *jobs)
-{
-	if (jobs->next_use)
-		jobs->next_use->prev_use = jobs->prev_use;
-	if (jobs->prev_use)
-		jobs->prev_use->next_use = jobs->next_use;
-}
+//static void		connect_use(t_jobs *jobs)
+//{
+//	if (jobs->next_use)
+//		jobs->next_use->prev_use = jobs->prev_use;
+//	if (jobs->prev_use)
+//		jobs->prev_use->next_use = jobs->next_use;
+//}
 
 void			memdel_jobs(t_jobs *jobs)
 {
-	t_jobs *temp_jobs;
+	t_jobs			*table;
+	int				index;
 
-	addr_jobs = jobs_table();
-	connect_use(jobs);
-	temp_jobs = *addr_jobs;
-	if (*addr_jobs == jobs)
-	{
-		dprintf(fd_log, "[%s] Memdel from addr\n", __FILENAME__);
-		dprintf(fd_log, "[%s] memdel [%d]\n", __FILENAME__, temp_jobs == jobs);
-		*addr_jobs = (*addr_jobs)->next;
-		if ((*addr_jobs) && (*addr_jobs)->prev)
-			(*addr_jobs)->prev = NULL;
-	}
-	else
-	{
-		while (jobs && temp_jobs && temp_jobs != jobs)
-		{
-			dprintf(fd_log, "[%s] memdel [%d]\n", __FILENAME__, temp_jobs == jobs);
-			temp_jobs = temp_jobs->next;
-		}
-		if (temp_jobs == jobs)
-		{
-			if (temp_jobs->next)
-				temp_jobs->next->prev = temp_jobs->prev;
-			if (temp_jobs->prev)
-				temp_jobs->prev->next = temp_jobs->next;
-		}
-	}
-	reset_process(&jobs);
+	table = jobs_table();
+	index = -1;
+	while (++index < MAX_CHILD - 1 && jobs != table)
+		table++;
+	reset_process(jobs);
+	ft_bzero(table, sizeof(t_jobs));
 }
