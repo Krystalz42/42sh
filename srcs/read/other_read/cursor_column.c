09d291e856f63f757206a39f00fa1c8c74f84fa0
krystalz42 <aroulin@student.42.fr>
2017-11-20 22:09:15 +0100

    Begin to Norme ! et Be ready for poush

--- srcs/read/other_read/cursor_column.c
+++ srcs/read/other_read/cursor_column.c
@@ -14,21 +14,21 @@
 
 int			cursor_column(int new_line)
 {
-//	char	buff[14];
-//	int		index;
-//	int		col;
-//
-//	ft_bzero(buff, 14);
-//	set_termios(SET_OUR_TERM);
-//	if ((write(STDIN_FILENO, CURSOR, sizeof(CURSOR))))
-//		read(STDIN_FILENO, buff, 14);
-//	index = 0;
-//	while (index < 14 && buff[index] != ';')
-//		index++;
-//	col = ft_atoi(buff + index + 1);
-//	dprintf(fd_log, "%s %d\n",buff, col);
-//	if (col > 2 && new_line)
-//		ft_putstr_fd("\x1B[7m%\x1B[0m\n", init_fd());
-//	return (col);
+	char	buff[14];
+	int		index;
+	int		col;
+
+	ft_bzero(buff, 14);
+	set_termios(SET_OUR_TERM);
+	if ((write(STDIN_FILENO, CURSOR, sizeof(CURSOR))))
+		read(STDIN_FILENO, buff, 14);
+	index = 0;
+	while (index < 14 && buff[index] != ';')
+		index++;
+	col = ft_atoi(buff + index + 1);
+	dprintf(fd_log, "%s %d\n",buff, col);
+	if (col > 2 && new_line)
+		ft_putstr_fd("\x1B[7m%\x1B[0m\n", init_fd());
+	return (col);
 	return (new_line);
 }
