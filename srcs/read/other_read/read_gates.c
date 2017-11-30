20d425f056c510e0fe163112edbb0b2ab71c3505
krystalz42 <aroulin@student.42.fr>
2017-11-30 15:04:08 +0100

    Hash -r fix

--- srcs/read/other_read/read_gates.c
+++ srcs/read/other_read/read_gates.c
@@ -17,6 +17,7 @@ void			initialize_fct(t_read **read_std, unsigned char flags)
 	(*read_std) = init_struct_for_read();
 	(*read_std)->print = 2;
 	init_prompt();
+	init_signal();
 	set_termios(SET_OUR_TERM);
 	(*read_std)->cur = prompt(flags | PRINT);
 	signal_reception(0);
