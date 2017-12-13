6633a5a67e7fd2e3ae4df7d490b35d2ec9db8808
Jefferson LE QUELLEC <jle-quel@e3r3p10.42.fr>
2017-12-13 22:57:45 +0100

    Update

--- srcs/execution/executing_function/op_great.c
+++ srcs/execution/executing_function/op_great.c
@@ -6,7 +6,7 @@
 /*   By: jle-quel <jle-quel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 15:19:54 by jle-quel          #+#    #+#             */
-/*   Updated: 2017/12/13 20:44:28 by jle-quel         ###   ########.fr       */
+/*   Updated: 2017/12/13 22:57:34 by jle-quel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,18 @@ static void		jobs_op_great(t_node *node)
 	else
 	{
 		fildes = check_fd(fildes);
-		std = STDOUT_FILENO;
-		if (ft_isdigit(node->content->command[0][0]))
-			std = ft_atoi(node->content->command[0]);
-		dup2(fildes, std);
+		if (ft_strchr(node->content->command[0], '&'))
+		{
+			dup2(fildes, STDOUT_FILENO);
+			dup2(fildes, STDERR_FILENO);
+		}
+		else
+		{
+			std = STDOUT_FILENO;
+			if (ft_isdigit(node->content->command[0][0]))
+				std = ft_atoi(node->content->command[0]);
+			dup2(fildes, std);
+		}
 	}
 	close(fildes);
 }
